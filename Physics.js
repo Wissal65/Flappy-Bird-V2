8ae6a68efef994031b8aec72d27b806be9585a4d
Wissal65 <wissalbara65@gmail.com>
2024-06-13 03:39:38 +0100

    Implement physics for game entities

---
+++ Physics.js
@@ -0,0 +1,42 @@
+import Matter from "matter-js";
+import { Dimensions } from 'react-native';
+
+const MAX_WIDTH = Dimensions.get("screen").width;
+const MAX_HEIGHT = Dimensions.get("screen").height;
+
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const Physics = (entities, { touches, time, dispatch }) => {
+    let engine = entities.physics.engine;
+    let world = entities.physics.world;
+
+    let hadTouches = false;
+    touches.filter(t => t.type === "press").forEach(t => {
+        if (!hadTouches){
+            if (world.gravity.y === 0.0){
+                world.gravity.y = 1.2;
+            }
+
+            hadTouches = true;
+        }
+
+    });
+
+    Matter.Engine.update(engine, time.delta);
+
+    Object.keys(entities).forEach(key => {
+        if (key.indexOf("floor") === 0){
+            if (entities[key].body.position.x <= -1 * MAX_WIDTH / 2){
+                Matter.Body.setPosition(entities[key].body, { x: MAX_WIDTH + (MAX_WIDTH / 2), y: entities[key].body.position.y });
+            } else {
+                Matter.Body.translate(entities[key].body, { x: -2, y: 0 });
+            }
+        }
+    });
+
+    return entities;
+};
+
+export default Physics;
