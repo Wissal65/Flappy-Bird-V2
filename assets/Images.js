4f55d1c455b421d2c5eaf84f2c1bf854c99d2051
Wissal65 <wissalbara65@gmail.com>
2024-06-14 23:42:06 +0100

    Add pipes images and update game assets

--- assets/Images.js
+++ assets/Images.js
@@ -2,4 +2,7 @@ export default Images = {
     background: require('./images/background.png'),
     floor: require('./images/floor.png'),
     bird: require('./images/bird.png'),
+    pipeCore: require('./images/pipe_core.png'),
+    pipeTop: require('./images/pipe_top.png'),
+    pipeBottom: require('./images/pipe_bottom.png'),
 }
