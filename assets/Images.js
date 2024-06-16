f957c1ce8becc7333c0b3d80e90f277f523d033a
Wissal65 <wissalbara65@gmail.com>
2024-06-16 23:20:29 +0100

    Added images to represent the bonus feature and adjusted references in Images.js

--- assets/Images.js
+++ assets/Images.js
@@ -7,9 +7,12 @@ export default Images = {
     // pipeTop: require('./images/pipe_top.png'),
     pipeTop: require('./images/pipe_top_v2.png'),
     pipeBottom: require('./images/pipe_bottom.png'),
+
     bonuses: [
-    require('./images/bonus1.png'), // e.g., Cloud of CO2
-    require('./images/bonus2.png'), // e.g., Cloud of NO2
-    // Add more bonus images as needed
+    require('./images/CO2.png'), // e.g., Cloud of CO2
+    require('./images/NO2.png'), // e.g., Cloud of NO2
+    require('./images/SO2.png'), // e.g., Cloud of NO2
+    // require('./images/O3.png'), // e.g., Cloud of O3
+   
   ],
 }
